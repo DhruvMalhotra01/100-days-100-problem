b7d52f8710b87d1572081b2eab9b701fb3aa4b80
Dhruv Malhotra <malhotradhruv112004@gmail.com>
2025-10-19 22:52:00 +0530

    Implement permutation generation using backtracking

---
+++ day1/BackTracking.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class BackTracking{
+    public static void printPermutations(String str, String perm, int idx){
+        if(str.length() == 0){
+            System.out.println(perm);
+            return;
+        }
+        for(int i = 0;i<str.length();i++){
+            char currChar = str.charAt(i);
+            String newStr = str.substring(0, i) + str.substring(i+1);
+            printPermutations(newStr, perm+ currChar, idx);
+            
+        }
+    }
+    public static void main(String[] args){
+        String str = "ABC";
+        printPermutations(str, "", 0);
+    }
+}
